0c308baa08494bd184b0554cbb1a84385926c4e1
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-25 23:46:42 +0530

    header section changes done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import logo from "../assets/images/site-logo.png";
+import headerBg from "../assets/images/homebg-img.jpg";
 
 function Header() {
   return (
-    <section className="container mx-auto">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold cursor-pointer text-pink-600">Madhur Events</h1>
-        <div className="flex gap-6 text-xl font-medium">
-          <a href="" className="text-gray-700 hover:text-pink-600 transition-colors">Home</a>
-          <a href="" className="text-gray-700 hover:text-pink-600 transition-colors">Profile</a>
-          <a href="" className="text-gray-700 hover:text-pink-600 transition-colors">Create Event</a>
-          <a href="" className="text-gray-700 hover:text-pink-600 transition-colors">Logout</a>
+    <section className="h-screen bg-cover bg-center bg-no-repeat py-4" style={{ backgroundImage: `url(${headerBg})` }}>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center px-6 py-3">
+          <div className="flex items-center gap-3">
+            <img src={logo} alt="logo" className="" />
+          </div>
+          <div className="flex gap-6 text-white font-medium">
+            <a href="" className="hover:text-blue-500">
+              About
+            </a>
+            <a href="" className="hover:text-blue-500">
+              Schedule
+            </a>
+            <a href="" className="hover:text-blue-500">
+              Artist
+            </a>
+            <a href="" className="hover:text-blue-500">
+              Contact
+            </a>
+          </div>
+        </div>
+        <div className="text-center text-white font-bold py-10">
+          <h2 className="text-5xl py-4">12-14 July / New York</h2>
+          <p className="text-8xl">The Ultimate Musical Extravaganza!</p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white px-10 py-4 my-12">BOOK TICKETS</button>
         </div>
       </div>
     </section>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <section className="container mx-auto">
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-pink-600">Welcome to Madhur Events</h1>
+            <p className="text-lg text-gray-700">Create and manage your events with ease</p>
+        </div>
+    </section>
   )
 }
 
